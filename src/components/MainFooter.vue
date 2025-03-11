8b05dd43013c5a0721e6a9db7a2486eeda8aa3b9
MaximOksiuta <63787095+MaximOksiuta@users.noreply.github.com>
2025-03-11 18:42:32 +0300

    feat: designed footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,12 +1,44 @@
 <template>
-    <div class="footer">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In cum aut, ipsum laboriosam distinctio voluptatibus
-            vel corrupti. Atque quibusdam reiciendis perspiciatis voluptates odit rerum doloremque nesciunt temporibus
-            tempora? Totam, labore nesciunt. Tempora omnis nostrum totam autem magnam, eius minima amet quibusdam saepe
-            quod explicabo iusto cum, nam recusandae, delectus blanditiis assumenda repellat ab incidunt veritatis
-            earum? Illum veritatis incidunt modi aut quisquam odit nulla sit est aspernatur hic dolor suscipit, facilis
-            eligendi alias mollitia vitae odio. Veritatis consectetur nobis, commodi ipsam minima soluta culpa
-            accusantium doloribus? Rem reprehenderit dolorem adipisci vel perspiciatis, ab mollitia, placeat, quasi
-            earum dicta distinctio reiciendis.</p>
+    <div class="footer max-width">
+        <div class="p-5 d-flex justify-content-center max-width bottom-border-cyan">
+            <div class="me-3" style="flex-grow: 1;">
+                <img src="../assets/big-logo-white.png" class="bottom-logo">
+                <p class="text-xs font-light on-footer-color mt-2">
+                    Цифровая проектная платформа<br>Университета «Сириус»
+                </p>
+            </div>
+
+            <div class="me-3" style="flex-grow: 1;">
+                <h3 class="white-color text-m font-regular pb-2 bottom-border-cyan">Контакты</h3>
+                <p class="text-xs font-light on-footer-accent-color mt-2 mb-2 me-4">
+                    Техническая поддержка
+                </p>
+                <p class="text-xs font-light on-footer-accent-color mt-2 mb-2 me-4">
+                    Email: <a class="none-deco text-xs font-light on-footer-accent-color"href="mailto:maximoksiuta@yandex.ru">maximoksiuta@yandex.ru</a>
+                </p>
+            </div>
+
+            <div class="me-3" style="flex-grow: 1;">
+                <h3 class="white-color text-m font-regular pb-2 bottom-border-cyan">АНОО ВО «УНИВЕРСИТЕТ «СИРИУС»</h3>
+                <a class="none-deco text-xs font-light on-footer-accent-color mt-2 mb-2 me-4"href="https://siriusuniversity.ru/">Сайт университета</a>
+            </div>
+        </div>
+        <p class="text-xs font-light on-footer-accent-color mt-4 mb-4 me-4 max-width text-center">
+            © 2025 Университет «Сириус». 
+        </p>
     </div>
 </template>
+
+<style>
+.bottom-logo {
+    max-height: 40px;
+}
+hr {
+  display: block;
+  height: 1px;
+  border: 0;
+  border-top: 1px solid red;
+  margin: 1em 0;
+  padding: 0;
+}
+</style>
